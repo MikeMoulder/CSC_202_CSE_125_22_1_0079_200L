36d32971e1f6913ee3e062dc375e4f3e4645e981
Michael Ogundeji - Mike Moulder <121914655+MikeMoulder@users.noreply.github.com>
2024-06-05 10:28:22 +0100

    Update Day 2.py

--- Day 2.py
+++ Day 2.py
@@ -6,6 +6,6 @@ tip = float(input("How much would you love to give as tip? "))
 
 total = bill + tip
 people =  int(input("How many people are paying? "))
-amount = round(total/people, 2)
+amount = round(total/people, 3)
 print(f"You have a bill of ${bill} and you are giving a tip of ${tip},"
 + f"also {people} people are paying, i.e each person will be paying ${amount}")
